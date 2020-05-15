62133635596c6a74a2cddcba8cdc2db9f37d583e
aidanreilly <aidreilly@gmail.com>
2020-05-15 15:16:51 +0100

    updates, works with metro

--- chords.lua
+++ chords.lua
@@ -1,42 +1,43 @@
 --- generate a chord from a single input 
--- in1: gate in
--- in2: chord root quantized to v/oct 
+-- in1: chord choice, 1v/oct chooses chord shape 1-12
+-- in2: chord root quantized to v/oct
 -- out1-4: chord degrees
 
---chords from music-thing-modular chord-organ
+-- choose your chord shape
+chords = { {0,0,0,0,0} --Root -5V
+, {0,4,7,12,0} --Major
+, {4,7,12,16,-5} --Major inv 1
+, {7,12,16,-5,0} --Major inv 2
+, {-12,-8,-5,0,4} --Major inv 3
+, {-8,-5,0,4,7} --Major inv 4
+, {0,4,7,11,0} --Major 7th
+, {4,7,11,0,16} --Major 7th inv 2 0V
+, {7,11,0,16,19} --Major 7th inv 3
+, {-12,-8,-5,-1,0} --Major 7th inv 4
+, {-8,4,7,11,23} --Major 7th no root
+, {0,0,0,0,0} --Root
+, {-24,-12,0,12,24} --organ
+, {-12,0,0,12,24} --2 up 1 down octave +5V
+}
 
-c1 = {0,4,7,12,0} --Major
-c2 = {4,7,12,16,-5} --Major inv 1
-c3 = {7,12,16,-5,0} --Major inv 2
-c4 = {-12,-8,-5,0,4} --Major inv 3
-c5 = {-8,-5,0,4,7} --Major inv 4
-c6 = {-5,0,4,7,12} --Major inv 5
-c7 = {0,4,7,11,0} --Major 7th
-c8 = {4,7,11,0,16} --Major 7th inv 2
-c9 = {7,11,0,16,19} --Major 7th inv 3
-c10 = {-12,-8,-5,-1,0} --Major 7th inv 4
-c11 = {-8,-5,-1,0,4} --Major 7th inv 5
-c12 = {-8,4,7,11,23} --Major 7th no root
-c13 = {0,0,0,0,0} --Root
-c14 = {-24,-12,0,12,24} --organ
-c15 = {-8,-5,4,7,16} --Major no root
-c16 = {-12,0,0,12,24} --2 up 1 down octave
-
---set the chord to play. also change this from druid
-chord = c1
---chords list
-chords = {c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12}
-
---clock in
 function init()
-	input[1]{mode = 'change', direction = 'rising'}
+	--use a metro to clock the sampling of in1 and in2
+	metro[1].event = create_chord
+	metro[1].time  = 0.005
+	metro[1]:start()
+	input[1].mode('none')
+    input[2].mode('none')
+    for n=1,4 do
+        output[n].slew = 0.001
+    end
 end
 
-input[1].change = function()
-	--get the voltage from input 2
-	v = input[2].volts
-	--/12 to get semitones in v/oct
+function create_chord()
+	--sel is used to pick out the chord from the chords list
+	sel = math.floor(input[2].volts/0.75 + 7.5)
+	root = input[1].volts
+	-- output chord degrees on each out1-4
 	for n=1,4 do
-		output[n].volts = (v + chord[n]/12)
-	end
+		output[n].volts = root + (chords[sel][n]/12)
+    end
 end
