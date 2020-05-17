e2fd1d21974800c5b636417d2a24fd8549e9e2f7
aidanreilly <aidreilly@gmail.com>
2020-05-17 01:07:23 +0100

    updates

--- chords.lua
+++ chords.lua
@@ -3,21 +3,22 @@
 -- in2: chord choice, send -5 to +5V
 -- out1-4: chord degrees
 
--- chord shapes!
-chords = { {0,0,0,0,0} --Root -5V
-, {1,5,8,13,1} --Major
-, {5,8,13,17,-4} --Major inv 1
-, {8,13,17,-4,1} --Major inv 2
-, {-11,-7,-4,1,5} --Major inv 3
-, {-7,-4,1,5,8} --Major inv 4
-, {1,5,8,11,1} --Major 7th
-, {5,8,12,1,17} --Major 7th inv 2 0V
-, {8,12,1,17,20} --Major 7th inv 3
-, {-11,-7,-4,0,1} --Major 7th inv 4
-, {-7,5,8,12,24} --Major 7th no root
-, {1,1,1,1,1} --Root
-, {-23,-11,1,13,25} --organ
-, {-11,1,1,15,25} --2 up 1 down octave +5V
+-- Try chords from here: https://github.com/monome/norns/blob/dev/lua/lib/musicutil.lua
+
+chords = { {0,0,0,0,0} --Root
+, {0,4,7,9,0} -- Maj6
+, {0,4,7,11,0} -- Maj7
+, {0,4,7,10,0} -- Dom7
+, {0,4,8,10,0} -- Aug7
+, {0,5,7,10,0} -- Sus4
+, {0,3,7,11,0} -- MinMaj7
+, {0,3,7,9,0} -- Min6
+, {0,3,7,10,0} -- Min7
+, {0,3,6,9,0} -- Dim7
+, {0,3,6,10,0} -- HalfDim7
+, {0,3,7,9,0} -- Min6
+, {0,3,7,10,0} -- Min7
+, {12,-12,12,-12,12} --OctUpDown
 }
 
 function init()
@@ -35,6 +36,7 @@ end
 function create_chord()
 	--sel is used to pick out the chord from the chords list
 	sel = math.floor(input[2].volts/0.75 + 7.5)
+	-- send 1v/oct into in1
 	root = input[1].volts
 	-- output chord degrees on each out1-4
 	for n=1,4 do
