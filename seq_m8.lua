332f14b4f39a8b878e249862c58c165ba017cf2c
aidanreilly <aidreilly@gmail.com>
2021-11-05 09:37:50 +0000

    updaytes

--- seq_m8.lua
+++ seq_m8.lua
@@ -2,10 +2,10 @@
 -- creates variations on an external sequence
 -- in1: sequencer 1v/oct
 -- in2: clock
--- out1: voice 2 1v/oct
--- out2: voice 2 trig
--- out3: bass 1v/oct
--- out4: bass trig
+-- out1: voice 1 1v/oct
+-- out2: voice 1 trig
+-- out3: voice 2 1v/oct
+-- out4: voice 2 trig
 
 notes = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 
@@ -23,13 +23,19 @@ function sequence()
   --make notes
   output[1].volts = seq_1()
   output[3].volts = seq_2()
-  --make gates
-  output[2](pulse())
-  output[4](pulse())
 end
 
+int_clk = clock.run(function()
+  while true do
+    clock.sync(1)
+    output[2]:clock(3) -- divide the clock
+    output[4]:clock(5)
+    sequence()
+  end
+end)
+
 function init()
-    input[2]{mode = 'change', direction = 'rising'}
+    input[2].mode('clock', 1)
     input[2].change = sequence
     print('seq m8 loaded m8')
 end
